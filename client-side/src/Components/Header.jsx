97b034d33bd91b3616a79657925ee20407bd84cd
Prachi Sirohi <psirohi061@gmail.com>
2025-02-22 11:57:45 +0530

    commit

--- client-side/src/Components/Header.jsx
+++ client-side/src/Components/Header.jsx
@@ -59,6 +59,10 @@ const Header = ({ onMenuToggle }) => {
 
   return (
     <>
+
+    {/* header  */}
+
+    {/* updated code */}
       <header className="bg-white border-b border-gray-300 px-6 py-2 fixed top-0 left-0 right-0 z-50">
         <div className="flex justify-between items-center">
           {/* Left Side: Logo and Menu Icon */}
